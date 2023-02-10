699bc5fc7150d1dcc09b32b93d51fd2b405f8365
AdamVicko <vickovic2203@gmail.com>
2023-02-10 19:51:54 +0100

    Implementing methods
    Functional love calculator with classes and methods. camelCase used.

--- Klasa.php
+++ Klasa.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 
 class Rekurzija
 {
+    //public function __construct()    
 
-    public function __construct(string $strBrojeva)
+    public function __construct($strBrojeva)
     {
+        $this->strBrojeva=$strBrojeva; // deklaracija za constructor jedna varijabla
+
         if(strlen($strBrojeva) < 3) // ako je dulkjina stringa manja od 100 vrati ga KRAJ
         {
             return $strBrojeva;
@@ -32,7 +35,63 @@ class Rekurzija
         }
         $zbroj = implode('',$zbroj);
 
-        return __construct($zbroj);
+        return Rekurzija::__construct($zbroj);
+    }
+
+    public function __tostring() //prebacivanje u string da mozemo ispisat ili dalje ucitat kao string
+    {
+        return "$this->strBrojeva";
+    }
+
+    static public function names()
+    {
+        $imeMomka = '';
+        $imeDjevojke = '';
+        echo PHP_EOL . PHP_EOL . 'LoveCalculator' . PHP_EOL . 'Use only spaces to seperate midle names' . PHP_EOL;
+        while(true)
+        {
+
+            $imeMomka = readline('Enter the name of the boy: ');
+            
+            if ( (preg_match('~[0-9]+~', $imeMomka)) || (str_contains($imeMomka, '_')) 
+                    || (str_contains($imeMomka, '-')) ) 
+                {
+                    continue;
+                }else 
+                {
+                    break;
+                }
+        }
+
+        while(true)
+        {    
+            $imeDjevojke = readline('Enter the name of the girl: ');
+
+            if ( (preg_match('~[0-9]+~', $imeDjevojke)) || (str_contains($imeDjevojke, '_')) 
+                    || (str_contains($imeDjevojke, '-')) ) 
+                {
+                    continue;
+                }else 
+                {
+                    break;
+                }
+        }
+
+        $imeMomkaLetter = str_split($imeMomka); //pretori string u array svaki character je novi
+        $imeDjevojkeLetter = str_split($imeDjevojke); //pretori string u array svaki character je novi
+        $obaImenaStr = $imeMomka . $imeDjevojke; //spajam imena
+        $obaImenaStr = strtolower($obaImenaStr);
+        $obaImenaStr = str_replace([' '],'',$obaImenaStr); // micemo razmake
+        //echo $obaImenaStr . PHP_EOL;
+        $obaImenaArr = str_split($obaImenaStr); //spojen imena astavljam u array
+        $final = [];
+        for($i=0 ; $i<(strlen($obaImenaStr)) ; $i++) //slova u brojeve 
+        {
+            $final[] = substr_count($obaImenaStr,$obaImenaArr[$i]);
+        }
+         // Vraćanje u string
+        $final = implode('',$final);
+        return $final;
     }
 
 }
--- index.php
+++ index.php
@@ -4,64 +4,7 @@
 declare(strict_types=1);
 include_once 'Klasa.php';
 
+    $finalnames = Rekurzija::names(); //pozovi gunkciju iz klase koja vraca spojena dva imena kao string
+    $rekurzija = new Rekurzija($finalnames); //taj string proslijedi u funkciju koja ce imena pretvoriti u brojeve i dati valjani postotak (string)
 
-$imeMomka = '';
-$imeDjevojke = '';
-
-echo PHP_EOL . PHP_EOL . 'LoveCalculator' . PHP_EOL . 'Use only spaces to seperate midle names' . PHP_EOL;
-while(true)
-{
-
-    $imeMomka = readline('Enter the name of the boy: ');
-    
-    if ( (preg_match('~[0-9]+~', $imeMomka)) || (str_contains($imeMomka, '_')) 
-            || (str_contains($imeMomka, '-')) ) 
-        {
-            continue;
-        }else 
-        {
-            break;
-        }
-}
-
-while(true)
-{    
-    $imeDjevojke = readline('Enter the name of the girl: ');
-
-    if ( (preg_match('~[0-9]+~', $imeDjevojke)) || (str_contains($imeDjevojke, '_')) 
-            || (str_contains($imeDjevojke, '-')) ) 
-        {
-            continue;
-        }else 
-        {
-            break;
-        }
-}
-
-    $imeMomkaLetter = str_split($imeMomka); //pretori string u array svaki character je novi
-    $imeDjevojkeLetter = str_split($imeDjevojke); //pretori string u array svaki character je novi
-
-    $obaImenaStr = $imeMomka . $imeDjevojke; //spajam imena
-    $obaImenaStr = strtolower($obaImenaStr);
-    $obaImenaStr = str_replace([' ','-','_'],'',$obaImenaStr); // micemo razmake
-
-
-    //echo $obaImenaStr . PHP_EOL;
-    $obaImenaArr = str_split($obaImenaStr); //spojen imena astavljam u array
-
-    $final = [];
-    for($i=0 ; $i<(strlen($obaImenaStr)) ; $i++) //slova u brojeve 
-    {
-        $final[] = substr_count($obaImenaStr,$obaImenaArr[$i]);
-    }
-
-    // Vraćanje u string
-    $final = implode('',$final);
-     //echo $final . PHP_EOL;
-    $rekurzija = new Rekurzija($final);
-     
-
-
-
-    echo PHP_EOL . 'THE CHANCES FOR TRUE LOVE ARE: ' . $rekurzija . '%';
-  
+echo PHP_EOL . 'THE CHANCES FOR TRUE LOVE ARE: ' . $rekurzija . '%';
